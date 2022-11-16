c7356092947469357ceae006893a100d4c2bb4a2
Kubadaleko <7757535@mail.ru>
2022-11-16 06:05:38 +0300

    001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// С клавиатуры вводится целое число. Вывести квадрат числа 
+int a;
+a = 10;
+System.Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+int b = a * a;
+System.Console.WriteLine($"{a}^2={b}");
+
+
---
+++ 009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести на экран четные числа от 1 до N
+
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    System.Console.Write($"{i} ");
+    i++;
+}
+
+for(int h=1;h<=N;h++)
+{
+ System.Console.Write($"{h} ");                                                                                         
+}
